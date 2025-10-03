a358094fea76208beae16e25d939c0672fba415d
Roma Leshok <leshokroma@gmail.com>
2025-10-03 18:35:35 +0300

    Task_9: applied code improvements

--- lab_2_2/task_9.py
+++ lab_2_2/task_9.py
@@ -7,11 +7,10 @@ def type_check(*types):
             return func(*args, **kwargs)
         return wrapper
     return decorator 
+#type_check = lambda *t: lambda f: lambda *a: len(a) == len(t) and all(type(x) == y for x, y in zip(a, t)) and f(*a) or exec('raise TypeError("Несоответствие типов")')
 
-
-@type_check(int , int )
-def test_func(a, b ): 
-    return a  + b
+@type_check(int ,int)
+def test_func(a, b): 
+    return a + b
 
 print(test_func(1,2))
-print(test_func("1" , "2"))
