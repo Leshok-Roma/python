4b211f006716fcd4ad7a9fd498b859d2c645f5d0
Roma Leshok <leshokroma@gmail.com>
2025-10-08 15:38:55 +0300

    task_7 was updated

--- task_7.py
+++ task_7.py
@@ -1,6 +1,6 @@
 input_seconds = int(input("Введите количество секунд: "))
 
 minutes = input_seconds // 60
-seconds = s % 60
+seconds = input_seconds % 60
 
 print(f"{input_seconds} секунд - {minutes} минут {seconds} секунд")
