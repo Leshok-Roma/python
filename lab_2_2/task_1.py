732f84a4f4fd9c0392dd65fafd0248f1cb16d942
Roma Leshok <leshokroma@gmail.com>
2025-11-05 16:20:13 +0300

    task_1

--- lab_2_2/task_1.py
+++ lab_2_2/task_1.py
@@ -2,15 +2,16 @@ def flatten_list(lst):
     def step(index):
         if index >= len(lst):
             return
-        if isinstance(lst[  index], list):
+        if isinstance(lst[index], list):
             current = lst.pop(index)
             lst[index:index] = current
-            helper(index)
+            step(index)  
         else:
-            helper(index + 1)
+            step(index + 1)
     step(0)
 
 
 lst = [1, 2, 3, [4], 5, [6, [7, [], 8, [9]]]]
-flatten_list(lst)
 
+flatten_list(lst)
+print(lst)
