a6bb630d74eedd18c036163568594f5882be46f6
Roma Leshok <leshokroma@gmail.com>
2025-12-17 12:37:50 +0300

    add chart

--- task.py
+++ task.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-
 df = pd.read_excel("s7_data_sample_rev4_50k.xlsx")
 
 print(df.columns)
 date_col = 'ISSUE_DATE'
-print(f" Используем колонку даты: {date_col}")
+print(f" date_col: {date_col}")
 
 df[date_col] = pd.to_datetime(df[date_col], errors="coerce")
 df["year"] = df[date_col].dt.year
 df["month"] = df[date_col].dt.month
+
 print(df.describe(include="all").transpose(), "\n")
 
 airport_counts = df["ORIG_CITY_CODE"].value_counts()
@@ -58,6 +57,32 @@ pay_counts.plot(kind="bar", color="purple")
 plt.title("Популярность способов оплаты")
 plt.ylabel("Количество")
 plt.grid(axis="y", alpha=0.4)
+plt.tight_layout()
+plt.show()
+
+
+
+top_cities_revenue = df.groupby("ORIG_CITY_CODE")["REVENUE_AMOUNT"] .sum() .sort_values(ascending=False).head(10)
+plt.figure(figsize=(10, 5))
+top_cities_revenue.sort_values().plot(kind="barh")
+plt.title("Топ-10 городов по суммарной выручке")
+plt.xlabel("Суммарная выручка")
+plt.ylabel("Город вылета")
+plt.grid(axis="x", alpha=0.4)
+plt.tight_layout()
+plt.show()
+
+daily_sales = df.groupby(df[date_col].dt.date)["REVENUE_AMOUNT"].sum()
+daily_sales_index = pd.to_datetime(daily_sales.index)
+rolling_mean = daily_sales.rolling(window=30).mean()
+exp_mean = daily_sales.ewm(span=30, adjust=False).mean()
+plt.figure(figsize=(10, 5))
+plt.plot(daily_sales, label="Фактические продажи")
+plt.plot(rolling_mean, label="Скользящее среднее (30 дней)")
+plt.plot(exp_mean, label="Экспоненциальное скользящее среднее (30 дней)")
+plt.legend()
+plt.title("Прогноз динамики продаж")
+plt.grid(True)
 plt.tight_layout()
 plt.show()
 
